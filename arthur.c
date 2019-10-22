3769cbaf59cf6f26ae6d3a4309a23a8c1fa645ff
Jejinketsu <Jederilson@gmail.com>
2019-10-22 16:13:06 -0300

    Busca em profundidade programada

--- arthur.c
+++ arthur.c
@@ -9,30 +9,34 @@ typedef struct {
 
 Grafo * criaGrafo(int vertices, int ehPonderado);
 void inserirAresta(Grafo **gr, int origem, int destino, int peso, int ehDigrafo);
+int buscaProfundidade(Grafo **gr, int origem, int *visitados, int cidades, int passos, int dinheiro);
+
+int contem(int *lista, int tam, int num);
 
 int main(){
     Grafo *grafo;
+    int *visitados = malloc(sizeof(int)*3);
+    int cidades = 0;
 
-    grafo = criaGrafo(3, 1);
+    grafo = criaGrafo(6, 1);
+    inserirAresta(&grafo, 1, 2, 3, 0);
+    inserirAresta(&grafo, 1, 5, 4, 0);
+    inserirAresta(&grafo, 1, 6, 6, 0);
+    inserirAresta(&grafo, 4, 6, 2, 0);
+    inserirAresta(&grafo, 4, 2, 5, 0);
+    inserirAresta(&grafo, 4, 3, 7, 0);
+    inserirAresta(&grafo, 2, 3, 2, 0);
+    inserirAresta(&grafo, 5, 6, 3, 0);
+
+    /*
     inserirAresta(&grafo, 1, 2, 4, 0);
     inserirAresta(&grafo, 2, 3, 4, 0);
     inserirAresta(&grafo, 3, 1, 4, 0);
+    */
 
-    printf("Arestas\n");
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            printf("%d ", grafo->arestas[j][i]);
-        }
-        printf("\n");
-    }
+    printf("cidades: %d\n", buscaProfundidade(&grafo, 1, visitados, 0, 0, 16));
 
-    printf("Pesos\n");
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            printf("%d ", grafo->pesos[j][i]);
-        }
-        printf("\n");
-    }
+    return 0;
 }
 
 Grafo * criaGrafo(int vertices, int ehPonderado){
@@ -76,4 +80,31 @@ void inserirAresta(Grafo **gr, int origem, int destino, int peso, int ehDigrafo)
             }
         }
     }
+}
+
+int buscaProfundidade(Grafo **gr, int origem, int *visitados, int cidades, int passos, int dinheiro){
+    if(gr != NULL) {
+        visitados[passos] = origem;
+
+        cidades = cidades > passos ? cidades : passos;
+
+        for(int i = 0; i < (*gr)->grau[origem-1]; i++){
+            if(!contem(visitados, passos, (*gr)->arestas[origem-1][i]) && dinheiro >= (*gr)->pesos[origem-1][i]){
+                int retorno = buscaProfundidade(gr, (*gr)->arestas[origem-1][i], visitados, cidades, passos+1, dinheiro-(*gr)->pesos[origem-1][i]);
+                cidades = cidades > retorno ? cidades : retorno;
+                visitados[passos+1] = 0;
+            }
+        }
+
+    }
+    return cidades;
+}
+
+int contem(int *lista, int tam, int num){
+    int contem = 0;
+
+    for(int i = 0; i < tam; i++)
+        if(lista[i] == num) contem = 1;
+    
+    return contem;
 }
